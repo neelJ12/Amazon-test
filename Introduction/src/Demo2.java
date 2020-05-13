4124d580a024d66703f9be629cff5e163036fa27
Manu Jakhar <jakharm@sheridancollege.ca>
2020-05-13 15:30:47 -0400

    test2

--- Introduction/src/Demo2.java
+++ Introduction/src/Demo2.java
@@ -1,9 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method st
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
@@ -11,6 +12,12 @@ public class Demo2 {
 		
 		driver.get("https://amazon.ca");
 		System.out.println(driver.getTitle());
+		Thread.sleep(2000);
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id=\"nav-search\"]/form/div[2]/div/input")).click();
+		Thread.sleep(2000);
+		//driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[2]/div/span[4]/div[1]/div[1]/div/span/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a")).click();
 	}
 
 }
